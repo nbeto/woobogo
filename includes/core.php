7a22f98f7df09d650016e3e3f47da95a5dec4228
Norberto <nbeto@users.noreply.github.com>
2025-07-16 17:27:19 +0100

    Update core.php

--- includes/core.php
+++ includes/core.php
@@ -3,3 +3,4 @@ defined('ABSPATH') || exit;
 
 // Carrega todas as regras ativas
 require_once WOOGOBO_PLUGIN_DIR . 'includes/rules/rule-2x1.php';
+require_once WOOGOBO_PLUGIN_DIR . 'includes/admin-settings.php';
